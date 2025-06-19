25ab156ea8f6835cbc8703098b827b57226f78a5
JoonHugh <joonhugh2021@gmail.com>
2025-06-19 18:36:31 +0900

    Please work last timee before I go home

--- frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -18,6 +18,13 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "function": {
+    "blogapi": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "lambda": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
